fb7d3ed21916f3848dfc5015305dc7cb4d650ab9
Jessabelle Lau <jessabelle.lausuba@edu.sait.ca>
2024-04-20 15:12:28 -0600

    first commit

---
+++ Javascript/recipe.js
@@ -0,0 +1,19 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+const date = new Date();
+let fullYear = date.getFullYear();
+document.getElementById("date").innerHTML = fullYear;
