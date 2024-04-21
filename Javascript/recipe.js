f945d395b4b3f60dee66e3a6c73997fbee590b2f
Jessabelle Lau <jessabelle.lausuba@edu.sait.ca>
2024-04-20 22:06:16 -0600

    interactive get recipe button

--- Javascript/recipe.js
+++ Javascript/recipe.js
@@ -14,6 +14,42 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+const Random = document.getElementById("random");
+
+Random.addEventListener("click", logRandomRecipe);
+
+async function logRandomRecipe(event) {
+    try {
+    const response = await
+    fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const RandomRecipe = await response.json();
+    displayRecipe(RandomRecipe.meals[0]);
+    } catch (error) {
+        console.error('Error fetching recipe', error);
+    }
+}
+
+function displayRecipe(recipe) {
+    recipeContainer.innerHTML = `
+        <h3 class="meal">${recipe.strMeal}</h3>
+        <h3 class="instr">Ingredients</h3>
+        <ul class="ingredient">
+            <li>${recipe.strIngredient1}</li>
+            <li>${recipe.strIngredient2}</li>
+            <li>${recipe.strIngredient3}</li>
+            <li>${recipe.strIngredient4}</li>
+            <li>${recipe.strIngredient5}</li>
+            <li>${recipe.strIngredient6}</li>
+            <li>${recipe.strIngredient7}</li>
+            <li>${recipe.strIngredient8}</li>
+            <li>${recipe.strIngredient9}</li>
+            <li>${recipe.strIngredient10}</li>
+        </ul>
+        <h3 class="instr">Instructions</h3>
+        <p class="instr">${recipe.strInstructions}</p>
+        `;
+}
+
 const date = new Date();
 let fullYear = date.getFullYear();
 document.getElementById("date").innerHTML = fullYear;
